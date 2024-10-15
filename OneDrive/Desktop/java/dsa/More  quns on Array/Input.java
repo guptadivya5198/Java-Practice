1a7f8d8556cf185221966579404728c2ae341453
guptadivya5198 <guptadivya5198@gmail.com>
2024-10-15 12:19:27 +0530

    array

---
+++ OneDrive/Desktop/java/dsa/More  quns on Array/Input.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter numbers");
+        int[] arr = new int[5];
+        for(int i = 0; i <arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+        // for(int i = 0; i <arr.length; i++){
+        //    System.out.print(arr[i] + " ");
+        // }
+
+        // for( int num : arr){
+        //     System.out.print(num);
+        // }
+    }
+}
